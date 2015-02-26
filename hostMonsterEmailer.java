e7c27d8f121b78be085c4f54560d68a900b40ad3
nikit <nikitph@gmail.com>
2015-02-25 17:52:30 -0800

    word frequency analyzer

--- hostMonsterEmailer.java
+++ hostMonsterEmailer.java
@@ -3,9 +3,9 @@ import com.twilio.sdk.TwilioRestException;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -16,6 +16,9 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import com.twilio.sdk.resource.factory.MessageFactory;
+import org.apache.pdfbox.exceptions.COSVisitorException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.util.PDFTextStripper;
 
 
 public class hostMonsterEmailer
@@ -24,8 +27,61 @@ public class hostMonsterEmailer
     public static final String ACCOUNT_SID = "ACac4953fc8d049aca76edcfe4197c30fd";
     public static final String AUTH_TOKEN = "082874fb2547132c67450817d67ac080";
 
-    public static void main(String[] args) throws TwilioRestException
+    public static void main(String[] args) throws TwilioRestException, IOException, COSVisitorException
     {
+
+
+
+        ArrayList<String> disallowedWords = new ArrayList<String>();
+        disallowedWords.add("the");
+        disallowedWords.add("a");
+        disallowedWords.add("an");
+        disallowedWords.add("and");
+        disallowedWords.add("for");
+        disallowedWords.add("i");
+        disallowedWords.add("but");
+        disallowedWords.add("because");
+        disallowedWords.add("of");
+        disallowedWords.add("it");
+        disallowedWords.add("was");
+        disallowedWords.add("he");
+        disallowedWords.add("that");
+        disallowedWords.add("in");
+        disallowedWords.add("you");
+        disallowedWords.add("to");
+        disallowedWords.add("his");
+        disallowedWords.add("s");
+        disallowedWords.add("t");
+
+
+        PDDocument pdf = PDDocument.load(new File("hp2.pdf"));
+
+        PDFTextStripper stripper = new PDFTextStripper();
+
+        String plainText = stripper.getText(pdf);
+
+        StringTokenizer st = new StringTokenizer(plainText," ,?;+=.\t\n\r\f\"\')([]|-");
+        TreeMap<String, Integer> textMap = new TreeMap<String, Integer>();
+        while(st.hasMoreTokens())
+        {
+            String temp = st.nextToken().toLowerCase();
+            if(disallowedWords.contains(temp))
+                continue;
+
+            if (textMap.containsKey(temp))
+            {
+                Integer x = textMap.get(temp);
+                int b = x.intValue() + 1;
+                textMap.replace(temp, x, (Integer) b);
+            } else
+                textMap.put(temp, 1);
+        }
+
+        TreeMap<Integer, String> inverseMap = new TreeMap<Integer, String>();
+        for (Map.Entry<String, Integer> entry : textMap.entrySet())
+        {
+            inverseMap.put(entry.getValue(), entry.getKey());
+        }
 
         final String username = "alpha@nikitph.com";
         final String password = "davajvmasd123";
@@ -48,12 +104,11 @@ public class hostMonsterEmailer
         {
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("nikitph@gmail.com"));
+            message.setFrom(new InternetAddress("alpha@nikitph.com"));
             message.setRecipients(Message.RecipientType.TO,
                     InternetAddress.parse("nikitph@gmail.com"));
             message.setSubject("Testing Subject");
-            message.setText("Dear Mail Crawler,"
-                    + "\n\n No spam to my email, please!");
+            message.setText("the most used word is : " + inverseMap.lastEntry().getValue() + " and occurs :" + inverseMap.lastEntry().getKey() +" times" + inverseMap.toString());
 
             Transport.send(message);
 
@@ -63,8 +118,8 @@ public class hostMonsterEmailer
 
             // Build a filter for the MessageList
             List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("Body", "love u poopi"));
-            params.add(new BasicNameValuePair("To", "+16502729074"));
+            params.add(new BasicNameValuePair("Body", "the most used word is : " + inverseMap.lastEntry().getValue() + " and occurs :" + inverseMap.lastEntry().getKey() +" times"));
+            params.add(new BasicNameValuePair("To", "+16502729075"));
             params.add(new BasicNameValuePair("From", "+13853991732"));
 
             MessageFactory messageFactory = client.getAccount().getMessageFactory();
